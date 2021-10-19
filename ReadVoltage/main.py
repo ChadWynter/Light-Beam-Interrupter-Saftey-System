eca1c28a45bfce0167e23b882c3ae8d7a85f3ee9
charliewolfe <cjwolfe@uiowa.edu>
2021-10-19 18:11:02 -0500

    listen to serial

---
+++ ReadVoltage/main.py
@@ -0,0 +1,26 @@
+import smtplib
+import serial
+
+'''
+verizon: @vtext.com
+att: @mms.att.net
+tmobile: @tmomail.net
+'''
+
+email = "ece.4880.lab@gmail.com"
+password = "KK5PF6qc9GKWhT"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(email, password)
+
+def send_sms(number, message):
+    server.sendmail(email, number, message)
+
+if __name__ == "__main__":
+    ser = serial.Serial("COM3", 9600, timeout=1)
+    while True:
+        if "indicator" in ser.readline().decode("utf-8"):
+            print("send sms")
+    # send_sms("6125997855@vtext.com", "test123")
+    
