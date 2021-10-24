4177ef3d2c887bac6dfdf2de9e3abf23549fdf86
Charlie Wolfe <cjwolfe@uiowa.edu>
2021-10-24 17:48:29 -0500

    detect filtered signal and output to led

--- ReadVoltage/ReadVoltage.ino
+++ ReadVoltage/ReadVoltage.ino
@@ -1,19 +1,60 @@
-int LED = 13;                                     //define LED digital pin 13
-int val = 0;                                         //define the voltage value of photo diode in digital pin 0
+int last_readings[10] = {0};
 
-
-void setup(){
-pinMode(LED,OUTPUT);                // Configure LED as output mode
-Serial.begin(9600);                         //Configure baud rate 9600
+void shift_and_append(int array[], int value, int array_size) {
+    if (!array_size)
+        return;
+    
+    for (int i = 0; i < array_size - 1; i++) 
+        array[i] = array[i+1];
+    
+    array[array_size - 1] = value;
 }
 
-void loop(){
-val = analogRead(0);                     // Read voltage value ranging from 0 -1023
-Serial.println(val);                         // read voltage value from serial monitor
-//if(val<1000){                                  // If lower than 1000, turn off LED
-//digitalWrite(LED,LOW);
-//}else{                                              // Otherwise turn on LED
-//digitalWrite(LED,HIGH);
-//}
-//delay(10);                                      // delay for 10ms
-//}
+int get_array_max_difference(int array[], int array_size) {
+    if (!array_size)
+        return 0.0f;
+    
+    int min = array[0], max = array[0];
+
+    for (int i = 1; i < array_size; i++) {
+        if (min > array[i])
+            min = array[i];
+
+        if (max < array[i])
+            max = array[i];
+    }
+
+    return max - min;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  int value = analogRead(A0);
+//  Serial.println(value);
+
+  shift_and_append(last_readings, value, 10);
+//  Serial.print("\n\n[");
+//  for(int i = 0; i < 10; i++) {
+//    Serial.print(last_readings[i]);
+//    Serial.print(" ");
+//  }
+//  Serial.print("]\n");
+//
+//  int diff = get_array_max_difference(last_readings, 10);
+//
+//  Serial.println(diff);
+  delay(100);
+
+  if (diff > 3) {
+//    Serial.println("led off");
+    digitalWrite(2, 0);
+  }
+  else {
+//    Serial.println("led on");
+    digitalWrite(2, 1);
+  }
+}
